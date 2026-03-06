145c6c98f4f8c84cbfa22126c4c6b448c53277d0
Cory Sanin <corysanin@outlook.com>
2026-03-05 23:21:09 -0500

    wtf

--- docker/scripts/entrypoint.sh
+++ docker/scripts/entrypoint.sh
@@ -2,6 +2,14 @@
 
 /scripts/pacman.conf.pl | sudo tee "/etc/pacman.conf" > /dev/null
 
+echo "REPO=$REPO"
+echo "DEP=$DEP"
+echo "TIER=$TIER"
+echo "POST=$POST"
+echo "FLAGS=$FLAGS"
+echo "username=$username"
+echo "MAKEPKGFLAGS=$MAKEPKGFLAGS"
+
 if [ -n "$MIRROR" ]
 then
     sudo sed -i "1iServer = $MIRROR" /etc/pacman.d/mirrorlist
@@ -42,7 +50,6 @@ doBuild() {
     export SOURCE_DATE_EPOCH=$(date +%s)
     FL="${FLAGS:--smf}"
     echo "makepkg $FL"
-    echo "  MAKEPKGFLAGS=$MAKEPKGFLAGS"
     if [ -n "$POST" ] && [ -x "./post-entrypoint.sh" ] ; then
         makepkg $FL --noconfirm --noprogressbar --skippgpcheck
     else
@@ -62,7 +69,6 @@ DIR="${WORKSPACE:-/home/user/pkg}"
 if [ ! -d "$DIR" ]; then
     git clone "$REPO" "$DIR" || exit $?
 fi
-echo "MAKEPKGFLAGS=$MAKEPKGFLAGS"
 cd "$DIR" && \
 checkoutCommit && \
 applyPatch && \
